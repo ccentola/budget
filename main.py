abcf55067f41b756a519d6c49b7d4a0d580a88c1
Carl Centola <carl.centola@gmail.com>
2021-01-05 17:13:53 -0700

    added ability to capture just last week's transactions

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import gspread
 from df2gspread import df2gspread as d2g
 from oauth2client.service_account import ServiceAccountCredentials
 from config import username, password
+from datetime import date, timedelta
 
 mint = mintapi.Mint(
     username,
     password,
     mfa_method='sms', # will need to try and automate
-    headless=False,
+    # headless=False,
+    headless=True,
     mfa_input_callback=None,
     session_path=None,
     imap_account=None,
@@ -29,6 +31,12 @@ transactions=transactions.drop(['labels','notes'],axis=1)
 transactions.loc[(transactions.transaction_type == 'debit'),'budget_category'] = 'Expense'
 transactions.loc[(transactions.transaction_type == 'credit'),'budget_category'] = 'Income'
 
+# last week's transactions
+today=pd.to_datetime('today').normalize()
+
+last_week = transactions[(transactions['date'] >= today - timedelta(weeks=1)) & (transactions['date'] <= today)]
+
+
 # https://docs.google.com/spreadsheets/d/1tIKaMNdMT3M2mz2ysHAUsvNZS2R_j8dXkCvF9cPOiiw/edit#gid=901716447
 
 # write to google sheets
@@ -36,7 +44,10 @@ scope=['https://spreadsheets.google.com/feeds']
 creds=ServiceAccountCredentials.from_json_keyfile_name('credentials_mint.json',scope)
 client=gspread.authorize(creds)
 spreadsheet_key='1tIKaMNdMT3M2mz2ysHAUsvNZS2R_j8dXkCvF9cPOiiw'
-d2g.upload(transactions,spreadsheet_key,'transactions_mint',credentials=creds,row_names=True)
+
+
+d2g.upload(transactions,spreadsheet_key,'transactions_mint',credentials=creds,row_names=False) # all transactions
+d2g.upload(last_week,spreadsheet_key,'last_week',credentials=creds,row_names=False) # last week's transactions
 
 # close session
 mint.close()
